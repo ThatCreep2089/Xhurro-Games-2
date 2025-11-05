153c7aad86e62b02886217ae4b0aa398cfb73354
victoEnano <vicmar20@ucm.es>
2025-11-05 21:18:51 +0100

    arreglada carga en git pages

--- src/scenes/bootScene.js
+++ src/scenes/bootScene.js
@@ -64,26 +64,26 @@ export default class Boot extends Phaser.Scene{
     }
     //Metodo para meter todos los recursos que necesitemos
     loadResources(){
-      this.load.image('map', '../assets/mainScene/map.png')
-      this.load.image('toni','../assets/imagenes/Toni.png')
-      this.load.image('map', '../assets/mainScene/map.png');
-      this.load.image('paint', '../assets/mainScene/paint.jpg');
-      this.load.image('destroyedHouse', '../assets/mainScene/destroyedHouse.jpg');
-      this.load.image('house', '../assets/mainScene/house.png');
-      this.load.image('buildSources', '../assets/mainScene/buildSources.jpg')
-      this.load.image('spaceKey', '../assets/mainScene/keyboard_space.png')
-      this.load.image('topo', '../assets/imagenes/boa.jpg');
-      this.load.image('hoyo', '../assets/imagenes/hole.png');
-      this.load.image('dinamita', '../assets/imagenes/bomba.png');
+      this.load.image('map', './assets/mainScene/map.png')
+      this.load.image('toni','./assets/imagenes/Toni.png')
+      this.load.image('map', './assets/mainScene/map.png');
+      this.load.image('paint', './assets/mainScene/paint.jpg');
+      this.load.image('destroyedHouse', './assets/mainScene/destroyedHouse.jpg');
+      this.load.image('house', './assets/mainScene/house.png');
+      this.load.image('buildSources', './assets/mainScene/buildSources.jpg')
+      this.load.image('spaceKey', './assets/mainScene/keyboard_space.png')
+      this.load.image('topo', './assets/imagenes/boa.jpg');
+      this.load.image('hoyo', './assets/imagenes/hole.png');
+      this.load.image('dinamita', './assets/imagenes/bomba.png');
 
       // === RECURSOS DESCRIPCIÓN MINIJUEGOS ===
-      this.load.video('WAMVideo', '../assets/WackAMole/WAMVideo.mp4')
-      this.load.image('MGInfoBG', '../assets/mainScene/MGInfoBG.jpg')
-      this.load.image('acceptButton', '../assets/mainScene/acceptButton.jpg')
-      this.load.image('refuseButton', '../assets/mainScene/refuseButton.jpg')
+      this.load.video('WAMVideo', './assets/WackAMole/WAMVideo.mp4')
+      this.load.image('MGInfoBG', './assets/mainScene/MGInfoBG.jpg')
+      this.load.image('acceptButton', './assets/mainScene/acceptButton.jpg')
+      this.load.image('refuseButton', './assets/mainScene/refuseButton.jpg')
 
       // === RECURSOS DIÁLOGOS ===
-      this.load.json('prueba','../data/DialogoPrueba.json')
+      this.load.json('prueba','./data/DialogoPrueba.json')
       this.first = false;
     }
 }
