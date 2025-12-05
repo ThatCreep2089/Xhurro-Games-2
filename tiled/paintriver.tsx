be2954843f74ca291401cbee86748509827eff3d
ThatCreep2089 <76827300+ThatCreep2089@users.noreply.github.com>
2025-12-05 10:36:14 +0100

    converted tiles to tiled files

---
+++ tiled/paintriver.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="paintriver" tilewidth="200" tileheight="200" tilecount="150" columns="15">
+ <image source="paintriver.png" width="3000" height="2000"/>
+</tileset>
---
+++ tiled/papeobstacles.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="papeobstacles" tilewidth="200" tileheight="200" tilecount="36" columns="6">
+ <image source="papeobstacles.png" width="1200" height="1200"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="12" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="13" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="14" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="15" wangid="0,0,0,0,1,1,1,0"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="18" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="19" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="20" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="21" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
