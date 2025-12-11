48a3b1a73766e2bacaf1d1e27e9fead730481102
Claudia <149375895+Clauudz@users.noreply.github.com>
2025-12-11 14:45:38 +0100

    imagenes dialogos actualizadas y resize y mejora de dialogos

--- src/scenes/bootScene.js
+++ src/scenes/bootScene.js
@@ -92,7 +92,7 @@ export default class Boot extends Phaser.Scene {
 
     this.load.image('toniPortrait', './assets/imagenes/NPC/ToniPortrait.png');
     this.load.image('pabloPortrait', './assets/imagenes/NPC/PabloPortrait.png');
-    this.load.image('ismaelPortrait', './assets/imagenes/NPC/IshmaelPortrait.png');
+    this.load.image('ishmaelPortrait', './assets/imagenes/NPC/IshmaelPortrait.png');
     this.load.image('cleonPortrait', './assets/imagenes/NPC/CleonPortrait.png');
     this.load.image('romePortrait', './assets/imagenes/NPC/RomePortrait.png');
     
